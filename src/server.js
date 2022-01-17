32f4cf7bad867407d3b73e16a0c2627d36bccf2a
Sérgio Félix <sergio98@outlook.pt>
2022-01-17 19:54:49 +0000

    fix

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 const config = require('./config/config')[process.env.NODE_ENV || 'development'];
 const app = require('./app');
 
-const server = app.listen(config.server.port, () => {
-	let host = server.address().address;
-	let port = server.address().port;
-	console.log(`Server running at -> http://${host}:${port}/`);
+app.listen(config.server.port, () => {
+	console.log(`Server running at -> http://${config.server.host}:${config.server.port}/`);
 });
